316fca4f381f880474610b36f5f6701c88e1a6f4
Frans Saris <frans@beech.it>
2017-01-03 15:39:46 +0100

    [TASK] Change upper dependency to allow TYPO3 CMS 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '7.6.4-7.6.99',
+                    'typo3' => '7.6.4-8.7.99',
                 ],
             'conflicts' =>
                 [
