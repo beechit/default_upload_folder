adef7ee9b83c9c411a1f7dd2c9bc89ac63a9564f
Georg Ringer <georgringer@users.noreply.github.com>
2020-11-25 15:05:36 +0100

    [TASK] Make compatible with 10
    Fixes #17

--- Classes/Hooks/DefaultUploadFolder.php
+++ Classes/Hooks/DefaultUploadFolder.php
@@ -1,9 +1,7 @@
 <?php
-
+declare(strict_types=1);
 namespace BeechIt\DefaultUploadFolder\Hooks;
 /*
- * This source file is proprietary property of Beech Applications B.V.
- * Date: 06-04-2016
  * All code (c) Beech Applications B.V. all rights reserved
  */
 
@@ -13,13 +11,13 @@ use TYPO3\CMS\Core\Resource\Exception\FolderDoesNotExistException;
 use TYPO3\CMS\Core\Resource\Folder;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Class DefaultUploadFolder
  */
 class DefaultUploadFolder
 {
+
     /**
      * Get default upload folder
      *
@@ -27,21 +25,15 @@ class DefaultUploadFolder
      * @param BackendUserAuthentication $backendUserAuthentication
      * @return Folder
      */
-    public function getDefaultUploadFolder($params, BackendUserAuthentication $backendUserAuthentication)
+    public function getDefaultUploadFolder($params, BackendUserAuthentication $backendUserAuthentication):Folder
     {
         /** @var Folder $uploadFolder */
         $uploadFolder = $params['uploadFolder'];
         $table = $params['table'];
         $field = $params['field'];
         $pageTs = BackendUtility::getPagesTSconfig($params['pid']);
-        $userTsConfig = $backendUserAuthentication->getTSConfig();
-        $subFolder = $this->getDefaultUploadFolderForTableAndField($table, $field, $pageTs, $userTsConfig);
-        if (trim($subFolder) === '') {
-            $subFolder = $this->getDefaultUploadFolderForTable($table, $pageTs, $userTsConfig);
-        }
-        if (trim($subFolder) === '') {
-            $subFolder = $this->getDefaultUploadFolderForAllTables($pageTs, $userTsConfig);
-        }
+
+        $subFolder = $pageTs['default_upload_folders.'][$table . '.'][$field] ?? $pageTs['default_upload_folders.'][$table] ?? '';
         // Folder by combined identifier
         if (preg_match('/[0-9]+:/', $subFolder)) {
             try {
@@ -52,54 +44,12 @@ class DefaultUploadFolder
                 // todo: try to create the folder
             }
         }
-        if (
-            $uploadFolder instanceof Folder
-            &&
-            $subFolder !== ''
-            &&
-            $uploadFolder->hasFolder($subFolder)
+
+        if ($subFolder && $uploadFolder instanceof Folder && $uploadFolder->hasFolder($subFolder)
         ) {
             $uploadFolder = $uploadFolder->getSubfolder($subFolder);
         }
+
         return $uploadFolder;
-    }
-
-    public function getDefaultUploadFolderForTableAndField(
-        $table,
-        $field,
-        array $defaultPageTs,
-        array $userTsConfig
-    )
-    {
-        $subFolder = $defaultPageTs['default_upload_folders.'][$table][$field] ?? '';
-        if (empty($subFolder)) {
-            $subFolder = $userTsConfig['default_upload_folders.'][$table][$field] ?? '';
-        }
-        return $subFolder;
-    }
-
-    protected function getDefaultUploadFolderForTable(
-        $table,
-        array $defaultPageTs,
-        array $userTsConfig
-    )
-    {
-        $subFolder = $defaultPageTs['default_upload_folders.'][$table] ?? '';
-        if (empty($subFolder)) {
-            $subFolder = $userTsConfig['default_upload_folders.'][$table] ?? '';
-        }
-        return $subFolder;
-    }
-
-    protected function getDefaultUploadFolderForAllTables(
-        array $defaultPageTs,
-        array $userTsConfig
-    )
-    {
-        $subFolder = $defaultPageTs['default_upload_folders.']['defaultForAllTables'] ?? '';
-        if (empty($subFolder)) {
-            $subFolder = $userTsConfig['default_upload_folders.']['defaultForAllTables'] ?? '';
-        }
-        return $subFolder;
     }
 }
