c2fa6661bd6a3322adb48fe0cd89909c957cfc05
Mathias Brodala <mbrodala@pagemachine.de>
2022-02-11 09:42:04 +0100

    [TASK] Update security guard constant
    See https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.0/Deprecation-92947-DeprecateTYPO3_MODEAndTYPO3_REQUESTTYPEConstants.html#typo3-mode-usage-as-global-script-file-security-gate

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['getDefaultUploadFolder'][] =
     \BeechIt\DefaultUploadFolder\Hooks\DefaultUploadFolder::class . '->getDefaultUploadFolder';
