beechit/default_upload_folder

commit d4c5e9a57fd0595c4bbadb7afa606d4b4784ed60

[TASK] Add upload folder check
If a backend user has no file mount at all and therefor no permission to access any folder it makes no sense to set a default upload folder. In such cases parameter uploadFolder is the empty string or null and method getDefaultUploadFolder should return immediatly.

Classes/Hooks/DefaultUploadFolder.php
@@ -23,10 +23,13 @@ class DefaultUploadFolder
      *
      * @param array $params
      * @param BackendUserAuthentication $backendUserAuthentication
-     * @return Folder
+     * @return Folder|null
      */
-    public function getDefaultUploadFolder($params, BackendUserAuthentication $backendUserAuthentication):Folder
+    public function getDefaultUploadFolder($params, BackendUserAuthentication $backendUserAuthentication):?Folder
     {
+        if (!($params['uploadFolder'] instanceof Folder)) {
+            return null;
+        }
         $rteParameters = $_GET['P'] ?? [];
         /** @var Folder $uploadFolder */
         $uploadFolder = $params['uploadFolder'];

commit 2fb7d7c3afacc7710785e3e8b5f9cc1e85da36eb

[BUGFIX] Fix regression introduced with d4c5e9a57f

Classes/Hooks/DefaultUploadFolder.php
@@ -25,10 +25,6 @@ class DefaultUploadFolder
      */
     public function getDefaultUploadFolder(array $params, BackendUserAuthentication $backendUserAuthentication): ?Folder
     {
-        if (! ($params['uploadFolder'] instanceof Folder)) {
-            return null;
-        }
-
         $rteParameters = $_GET['P'] ?? [];
 
         /** @var Folder $uploadFolder */
@@ -65,7 +61,7 @@ class DefaultUploadFolder
             $uploadFolder = $uploadFolder->getSubfolder($subFolder);
         }
 
-        return $uploadFolder;
+        return ($uploadFolder instanceof Folder) ? $uploadFolder : null;
     }
 
     /**